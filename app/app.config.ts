cfa157c2f55d8c3ac932461725a51e75c6cbb585
Mateo Vuković <231365949+roadtovuki@users.noreply.github.com>
2026-01-02 20:18:03 +0100

    Completely finish hero section

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,20 @@
 export default defineAppConfig({
   ui: {
     colors: {
-      primary: 'gray',
-      neutral: 'zinc',
+      primary: 'stone',
+      neutral: 'stone',
+    },
+    button: {
+      variants: {
+        size: {
+          '2xl': {
+            base: 'px-4 py-3 text-md gap-3',
+            leadingIcon: 'size-5',
+            leadingAvatarSize: '2xs',
+            trailingIcon: 'size-5',
+          },
+        },
+      },
     },
   },
 });
--- app/components/AppHero.vue
+++ app/components/AppHero.vue
@@ -1,22 +1,39 @@
 <template>
-  <div class="h-screen relative">
+  <div class="h-screen relative overflow-hidden">
     <video
       src="/hero.mp4"
       autoplay
       loop
       muted
+      playsinline
       class="h-full w-full brightness-50 object-cover"
     ></video>
-    <div class="absolute top-2/5 left-1/8">
-      <h1 class="text-gray-50 font-bold text-6xl mb-6">
-        Arterior
-        <br />
-        Design with a dose of art
-      </h1>
-      <div class="flex gap-4">
-        <UButton size="xl">Start your project</UButton>
-        <UButton size="xl" variant="outline">View our work</UButton>
-      </div>
+
+    <div class="absolute inset-0 flex items-center">
+      <UContainer class="w-full">
+        <div class="max-w-4xl px-4 sm:px-0 sm:ml-[10%]">
+          <h1
+            class="text-gray-50 font-bold text-5xl md:text-6xl lg:text-7xl mb-8 leading-[1.1]"
+          >
+            Arterior
+            <br />
+            Design with a dose of art
+          </h1>
+
+          <div class="flex flex-col sm:flex-row gap-4">
+            <UButton size="2xl" class="justify-center">
+              Start your project
+            </UButton>
+            <UButton
+              size="2xl"
+              variant="outline"
+              class="ring-1 ring-white/40 text-white backdrop-blur-xs justify-center"
+            >
+              View our work
+            </UButton>
+          </div>
+        </div>
+      </UContainer>
     </div>
   </div>
 </template>
